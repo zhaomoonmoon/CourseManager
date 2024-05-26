7485b4b3026450863d91a766f56330bcb3c53c18
moon <yqmkami@outlook.com>
2024-05-26 22:36:08 +0800

    Student Course Selection Management System

---
+++ CourseManagerVue/src/views/student/StudentTimeTable.vue
@@ -0,0 +1,37 @@
+<!--
+ * @Description: 
+ * @Author: Rabbiter
+ * @Date: 2023-06-06 09:09:08
+-->
+<template>
+    <div class="timetable-wrap">
+        <div class="crumbs">
+            <el-breadcrumb separator="/">
+                <el-breadcrumb-item>
+                    <b style="font-size: 22px"> 课表查询</b>
+                </el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+
+        <div class="container">
+            <time-table ref="timeTable"></time-table>
+        </div>
+    </div>
+</template>
+
+<script>
+import * as api from "../../api/student/timetable";
+import TimeTable from "../../components/TimeTable";
+
+export default {
+    name: "StudentTimeTable",
+    components: { TimeTable },
+    created() {
+        api.get().then((res) => {
+            this.$refs.timeTable.updateData(res);
+        });
+    },
+};
+</script>
+
+<style scoped></style>
---
+++ CourseManagerVue/src/views/teacher/TeacherTimetable.vue
@@ -0,0 +1,37 @@
+<!--
+ * @Description: 
+ * @Author: Rabbiter
+ * @Date: 2023-06-06 09:09:08
+-->
+<template>
+  <div class="timetable-wrap">
+    <div class="crumbs">
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item>
+          <b style="font-size: 22px;"> 教师课表</b>
+        </el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+
+    <div class="container">
+      <time-table ref="timeTable"></time-table>
+    </div>
+  </div>
+</template>
+
+<script>
+import * as api from "../../api/teacher/timetable";
+import TimeTable from "../../components/TimeTable";
+
+export default {
+  name: "TeacherTimetable",
+  components: { TimeTable },
+  created() {
+    api.get().then(res => {
+      this.$refs.timeTable.updateData(res);
+    });
+  }
+};
+</script>
+
+<style scoped></style>
